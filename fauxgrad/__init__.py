6262333850c1de843b802ce9ba9c494a947078e9
Kiran Sanjeevan <ksanjeevancabeza@gmail.com>
2021-09-07 22:44:34 -0700

    refactor

--- fauxgrad/__init__.py
+++ fauxgrad/__init__.py
@@ -1,2 +1,7 @@
 from .value import Value
 from .nn import Linear
+
+def cross_entropy(pred, target):
+    # this is a bit ugly, as it's at the expense of not implementing __sub__, etc.
+    ret = [-p.log() if t == 1 else -(-p+1).log() for p, t in zip(pred, target)]
+    return sum(ret)
--- fauxgrad/utils.py
+++ fauxgrad/utils.py
@@ -30,37 +30,36 @@ def plot_graph(value):
 
 
 def get_example_graph():
-  from fauxgrad import Value
   from test.test_value import graph1
   return graph1(Value)[-1]
 
 # Generate toy 2D data of a donut inside another donut
 def generate_circles(num_samples):
-    from sklearn.datasets import make_circles
-    X, Y = make_circles(n_samples=num_samples, factor=.4, noise=.11)
-    return X, Y.reshape(-1,1)
+  from sklearn.datasets import make_circles
+  X, Y = make_circles(n_samples=num_samples, factor=.4, noise=.11)
+  return X, Y.reshape(-1,1)
 
 # Visualize the contour plot of the neural net's predictions
 def visualize_plot(X, Y, m, title=None):
-    resolution = 100
-    xmin, xmax = X[:,0].min(), X[:,0].max()
-    ymin, ymax = X[:,1].min(), X[:,1].max()
+  resolution = 100
+  xmin, xmax = X[:,0].min(), X[:,0].max()
+  ymin, ymax = X[:,1].min(), X[:,1].max()
 
-    x = np.linspace(xmin, xmax, resolution)
-    y = np.linspace(ymin, ymax, resolution)
-    xx, yy = np.meshgrid(x, y)
+  x = np.linspace(xmin, xmax, resolution)
+  y = np.linspace(ymin, ymax, resolution)
+  xx, yy = np.meshgrid(x, y)
 
-    d = np.stack([xx, yy], axis=2).reshape(-1,2)
-    
-    z = np.array([m([Value(d[0]), Value(d[1])])[0].val for d in d]).reshape(resolution, resolution)
-    
-    #plt.contourf(x,y,z, alpha=0.4, cmap='RdYlBu')
-    #plt.scatter(X[:,0], X[:,1], c=np.squeeze(Y), cmap='bwr_r', s=30, alpha=0.6)
-    plt.contourf(x,y,z, alpha=0.4, cmap='plasma')
-    plt.scatter(X[:,0], X[:,1], c=np.squeeze(Y), cmap='binary', s=30, alpha=0.6)
+  d = np.stack([xx, yy], axis=2).reshape(-1,2)
+  
+  z = np.array([m([Value(d[0]), Value(d[1])])[0].val for d in d]).reshape(resolution, resolution)
+  
+  #plt.contourf(x,y,z, alpha=0.4, cmap='RdYlBu')
+  #plt.scatter(X[:,0], X[:,1], c=np.squeeze(Y), cmap='bwr_r', s=30, alpha=0.6)
+  plt.contourf(x,y,z, alpha=0.4, cmap='plasma')
+  plt.scatter(X[:,0], X[:,1], c=np.squeeze(Y), cmap='binary', s=30, alpha=0.6)
 
-    ax = plt.gca()
-    ax.set_facecolor((0.95, 0.95, 0.95))
-    if title is not None: ax.set_title(title)
-    plt.axis('off')
-    plt.show()
+  ax = plt.gca()
+  ax.set_facecolor((0.95, 0.95, 0.95))
+  if title is not None: ax.set_title(title)
+  plt.axis('off')
+  plt.show()
