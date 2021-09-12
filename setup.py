9c2037605e6dbbd327d0ff7960f4a7a02bec458f
Kiran Sanjeevan <ksanjeevancabeza@gmail.com>
2021-09-12 14:01:47 -0700

    update

--- setup.py
+++ setup.py
@@ -1,23 +1,35 @@
 
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="fauxgrad",
-    version='0.1',
-    author='Kiran Sanjeevan',
-    url='http://github.com/ksanjeevan/fauxgrad',
-    author_email="ksanjeevancabeza@gmail.com",
-    description="A crappy automatic differentiation lilbrary",
-    packages=['fauxgrad'],
-    license='MIT',
-    install_requires=[
-        'micrograd',
-        'pytest',
-        'matplotlib',
-        'numpy',
-        'networkx',
-        'sklearn',
-      ],
-    keywords="automatic differentiation python",
-    zip_safe=False
+  name="fauxgrad",
+  version='0.1',
+  author='Kiran Sanjeevan Cabeza',
+  url='http://github.com/ksanjeevan/fauxgrad',
+  author_email="ksanjeevancabeza@gmail.com",
+  description="A walkthrough of a small engine for automatic differentiation",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  packages=['fauxgrad'],
+  license='MIT',
+  install_requires=[
+    'micrograd',
+    'pytest',
+    'matplotlib',
+    'numpy',
+    'networkx',
+    'sklearn',
+    'ipython>=7.21.0'
+    ],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  keywords="autodiff automatic differentiation python",
+  zip_safe=False,
+  python_requires=">=3.6",
 )
